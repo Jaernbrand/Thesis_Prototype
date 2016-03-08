d88a79d14d1c1332ee2c9281699830e0d31c0a4f
Jaernbrand <henrikjarnbrand@gmail.com>
2016-03-08 16:57:07 +0100

    Create Setup.js

---
+++ js/Setup.js
@@ -0,0 +1,24 @@
+/**
+* Setup for the user tests of the prototype.
+*/
+
+
+/**
+* Fills the database with data.
+*/
+function fillDatabase(){
+	SimpleText.database.addAuthor("Nisse");
+	SimpleText.database.addAuthor("Fred");
+	SimpleText.database.addAuthor("óle");
+
+	var quest1 = "quest1";
+	var contId = SimpleText.database.addContribution(quest1, "óle", "Try jumping.");
+
+	SimpleText.database.addComment(quest1, contId, "Fred", "You can't jump in this game.");
+
+	SimpleText.database.addPicture(quest1, contId, "path/picture");
+	SimpleText.database.addSound(quest1, contId, "path/picture");
+};
+
+fillDatabase();
+
