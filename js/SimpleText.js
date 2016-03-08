35e70d3556fea760df9676119440dcedf6694ed6
Tomas <tomassandberg86@hotmail.com>
2016-03-08 17:11:25 +0100

    html och css layout

--- js/SimpleText.js
+++ js/SimpleText.js
@@ -7,9 +7,24 @@ document.getElementById("okButton").onclick = function () {
 	document.getElementById("simpleTextStart").style.visibility = "hidden";
 };
 
+// Open contribution list
+document.getElementById("simpleTextButton").onclick = function () { 
+	document.getElementById("simpleTextStart").style.visibility = "hidden";
+	document.getElementById("contributions").style.visibility = "visible";
+};
 
 
+// Go back to simpleTextStart from contribution list
+document.getElementById("contributeBack").onclick = function () { 
+	document.getElementById("contributions").style.visibility = "hidden";
+	document.getElementById("simpleTextStart").style.visibility = "visible";
+	
+};
 
+function test(){
+	alert("Hello");
+	console.log("hej");
+}
 
 
 
