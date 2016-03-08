0a4c0dfa0606e6c15385f47418f49d511d4beb47
Jaernbrand <henrikjarnbrand@gmail.com>
2016-03-08 16:56:29 +0100

    Create Database.js

---
+++ js/Database.js
@@ -0,0 +1,326 @@
+/**
+* Stores and retrieves information related to
+* SimpleText.
+*/
+
+/**
+* @class SimpleText.Database
+*
+* Constructor for SimpleText.Database.
+*/
+SimpleText.Database = function(){
+
+	/**
+	* @property object authors
+	*/
+	this.authors = {}
+
+	/**
+	* @property object contributions
+	*/
+	this.contributions = {};
+};
+
+/**
+* Adds the given author name to the database.
+*
+* @param string name
+* - name of the author.
+*/
+SimpleText.Database.prototype.addAuthor = function(name){
+	if (typeof name !== "string"){
+		throw "name: Invalid type " + (typeof name) +
+				" Expected string.";
+	}
+
+	/**
+	* @struct Author
+	*
+	* @property string name
+	* @property int favourited
+	*/
+	this.authors[name] = {
+		name: name,
+		favourited: 0
+	};
+};
+
+/**
+* Decrements the number of users that favourited 
+* the author.
+*
+* @param string name
+* - name of the author to defavourite.
+*/
+SimpleText.Database.prototype.defavouriteAuthor = function(name){
+	if (typeof name !== "string"){
+		throw "name: Invalid type " + (typeof name) +
+				" Expected string.";
+	}
+
+	this.authors[name].favourited--;
+};
+
+/**
+* Increments the number of users that favourited 
+* the author.
+*
+* @param string name
+* - name of the author to favourite.
+*/
+SimpleText.Database.prototype.favouriteAuthor = function(name){
+	if (typeof name !== "string"){
+		throw "name: Invalid type " + (typeof name) +
+				" Expected string.";
+	}
+
+	this.authors[name].favourited++;
+};
+
+/**
+* Adds a contribution to the database. The contribution
+* is a simplified text of the quest text with the given
+* quest id.
+*
+* @param string questID
+* - the id of the quest for which to add a contribution.
+*
+* @return string
+* - id of the newly added contribution.
+*/
+SimpleText.Database.prototype.addContribution = function(questID, author, text){
+	if (typeof questID !== "string"){
+		throw "questID: Invalid type " + (typeof questID) +
+				" Expected string.";
+	}
+
+	if (!this.contributions[questID]){
+		this.contributions[questID] = []
+	}	
+
+	var id = this.contributions[questID].length.toString();
+
+	/**
+	* @struct Contribution
+	*
+	* @property string contID
+	* @property string author
+	* @property int votes
+	* @property int flags
+	* @property array comments
+	* @property string text
+	* @property array pictures,
+	* @property array sounds
+	*/
+	this.contributions[questID].push({
+		contID: id,
+		author: author,
+		votes: 0,
+		flags: 0,
+		comments: [],
+		text: text,
+		pictures: [],
+		sounds: []
+	});
+
+	return id;
+};
+
+/**
+* Adds a path to a pictrue to the contribution with 
+* the specified contribution ID and for the quest with
+* the specified quest ID.
+*
+* @param string questID
+* - the ID of the quest to which the contribution belongs.
+*
+* @param string contID
+* - the ID of the contribution for which to add the sound file.
+*
+* @param string picture
+* - path to the picture.
+*/
+SimpleText.Database.prototype.addPicture = function(questID, contID, picture){
+	if (typeof questID !== "string"){
+		throw "questID: Invalid type " + (typeof questID) +
+				" Expected string.";
+	}
+	if (typeof contID !== "string"){
+		throw "contID: Invalid type " + (typeof contID) +
+				" Expected string.";
+	}
+	if (typeof picture !== "string"){
+		throw "picture: Invalid type " + (typeof picture) +
+				" Expected string.";
+	}
+
+	for(var i=0; i < this.contributions[questID].length; ++i){
+		if (this.contributions[questID][i].contID === contID){
+			this.contributions[questID][i].pictures.push(picture);
+			break;
+		}
+	}
+}
+
+/**
+* Adds a path to a sound file to the contribution with 
+* the specified contribution ID and for the quest with
+* the specified quest ID.
+*
+* @param string questID
+* - the ID of the quest to which the contribution belongs.
+*
+* @param string contID
+* - the ID of the contribution for which to add the sound file.
+*
+* @param string sound
+* - path to the sound file.
+*/
+SimpleText.Database.prototype.addSound = function(questID, contID, sound){
+	if (typeof questID !== "string"){
+		throw "questID: Invalid type " + (typeof questID) +
+				" Expected string.";
+	}
+	if (typeof contID !== "string"){
+		throw "contID: Invalid type " + (typeof contID) +
+				" Expected string.";
+	}
+	if (typeof sound !== "string"){
+		throw "sound: Invalid type " + (typeof sound) +
+				" Expected string.";
+	}
+
+	for(var i=0; i < this.contributions[questID].length; ++i){
+		if (this.contributions[questID][i].contID === contID){
+			this.contributions[questID][i].sounds.push(sound);
+			break;
+		}
+	}
+}
+
+/**
+* Adds a comment to the contribution for the quest
+* with the given quest ID and with the given 
+* contribution ID.
+*
+* @param string questID
+* - the ID of the quest to which the contribution belongs.
+*
+* @param string contID
+* - the ID of teh contribution for which to add the comment.
+*
+* @param string author
+* - the author of the comment.
+*
+* @param string text
+* - the comment text. 
+*
+* @return string
+* - the comment id of the newly added comment.
+*/
+SimpleText.Database.prototype.addComment = function(questID, contID, author, text){
+	if (typeof questID !== "string"){
+		throw "questID: Invalid type " + (typeof questID) +
+				" Expected string.";
+	}
+	if (typeof contID !== "string"){
+		throw "contID: Invalid type " + (typeof contID) +
+				" Expected string.";
+	}
+
+	var id; 
+
+	console.log(this.contributions[questID]);
+	console.log(this.contributions[questID].length);
+
+	for(var i=0; i < this.contributions[questID].length; ++i){
+		if (this.contributions[questID][i].contID === contID){
+			id = this.contributions[questID][i].comments.length.toString();
+	
+			/**
+			* @struct Comment
+			*
+			* @property string comID
+			* @property string author
+			* @property string text
+			*/
+			this.contributions[questID][i].comments.push({
+				comID: id,
+				author: author,
+				text: text
+			});
+			break;
+		}
+	}
+
+	return id;
+};
+
+/**
+* Fetches the author with the specified name.
+*
+* @param string name
+* - name of the author to fetch.
+*
+* @return object
+* - object representing an author.
+*/
+SimpleText.Database.prototype.fetchAuthor = function(name){
+	if (typeof name !== "string"){
+		throw "name: Invalid type " + (typeof name) +
+				" Expected string.";
+	}
+	
+	return this.authors[name] || null;
+};
+
+/**
+* Fetches the contributions for the supplied quest ID.
+*
+* @param string questID
+* - the ID of the quest for which to fetch the contributions.
+*
+* @return array
+* - the quest contributions.
+*/
+SimpleText.Database.prototype.fetchContributions = function(questID){
+	if (typeof questID !== "string"){
+		throw "questID: Invalid type " + (typeof questID) +
+				" Expected string.";
+	}
+
+	return this.contributions[questID] || [];
+};
+
+/**
+* Fetches the comments for the quest with the given quest ID
+* and with the given contribution ID.
+*
+* @param string questID
+* - the quest ID for which to fetch the comments.
+*
+* @param string contID
+* - the contributions ID for which to fetch the comments.
+*
+* @return array
+* - the comments for the contribution to the quest.
+*/
+SimpleText.Database.prototype.fetchComments = function(questID, contID){
+	if (typeof questID !== "string"){
+		throw "questID: Invalid type " + (typeof questID) +
+				" Expected string.";
+	}
+	if (typeof contID !== "string"){
+		throw "contID: Invalid type " + (typeof contID) +
+				" Expected string.";
+	}
+
+	return this.contributions[questID][contID] || [];
+};
+
+
+
+SimpleText.database = new SimpleText.Database();
+
+
+
