5359fb28fad05e9f51575b68a0bbe2285502b42f
Jaernbrand <henrikjarnbrand@gmail.com>
2016-03-16 15:07:12 +0100

    Populate database with comments

--- js/UserTestSetup.js
+++ js/UserTestSetup.js
@@ -23,13 +23,35 @@ function fillDatabase(){
 											"to have a map, but I lost it somewhere. Please, help me " + 
 											"find my map.");
 
+		var comNames = ["óle", "Fred", "Hugo"];
+		var comTexts = ["Where can I find the map?",
+					"Some of those words seems a little advanced...",
+					"Great simplification!"];
 		for (var i=0; i < 3; ++i){
 			SimpleText.database.addVote(questID, nisseContId);
+			SimpleText.database.addComment(questID, nisseContId, comNames[i], comTexts[i]);
 		}
 
 		var oleContId = SimpleText.database.addContribution(questID, "óle", "Try jumping.");
 		SimpleText.database.addComment(questID, oleContId, "Fred", "You can't jump in this game.");
-	}
+		SimpleText.database.addComment(questID, oleContId, "Hugo", "This has nothing to do with the quest.");
+
+
+		fredContId = SimpleText.database.addContribution(questID, 
+														"Fred", 
+						"I'm lost. Please find my map.");
+
+		var fComNames = ["Nisse", "Hugo", "óle"];
+		var fComTexts = ["You ignored a lot of information.",
+						"Straight to the point!",
+						"Try jumping."];
+		for (var j=0; j < 4; ++j){
+			if (j % 2){
+				SimpleText.database.addVote(questID, fredContId);
+			}
+			SimpleText.database.addComment(questID, nisseContId, fComNames[i], fComTexts[i]);
+		}
+	 } // populateQuest1
 
 	populateAuthors();
 	populateQuest1("quest1");
@@ -39,7 +61,7 @@ function fillDatabase(){
 	SimpleText.database.addPicture(quest1, contId, "path/picture");
 	SimpleText.database.addSound(quest1, contId, "path/picture");
 	*/
-};
+}; // fillDatabase
 
 fillDatabase();
 
