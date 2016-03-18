c8632cbb1f90ff16aceebcf65286fd40f9f79c22
Jaernbrand <henrikjarnbrand@gmail.com>
2016-03-18 16:17:48 +0100

    Add fake contributions

--- js/UserTestSetup.js
+++ js/UserTestSetup.js
@@ -17,7 +17,16 @@ function fillDatabase(){
 		SimpleText.database.addAuthor("óle");
 	}
 
+	/**
+	* Populates database with contributions for quest 1.
+	*
+	* @param [string] questID
+	* Quest ID for quest 1.
+	*/
 	function populateQuest1(questID){
+		// ===== //
+		// Nisse //
+		// ===== //
 		nisseContId = SimpleText.database.addContribution(questID, "Nisse", "Please, traveler. I need help.\n\n" + 
 											"I'm lost. Oh, I should've never left our camp. I used " +
 											"to have a map, but I lost it somewhere. Please, help me " + 
@@ -34,12 +43,18 @@ function fillDatabase(){
 			SimpleText.database.addComment(questID, nisseContId, comNames[i], comTexts[i]);
 		}
 
+		// === //
+		// óle //
+		// === //
 		var oleContId = SimpleText.database.addContribution(questID, "óle", "Try jumping.");
 		SimpleText.database.addComment(questID, oleContId, "Fred", "You can't jump in this game.");
 		SimpleText.database.addComment(questID, oleContId, "Hugo", "This has nothing to do with the quest.");
 
 		SimpleText.database.addSound(questID, oleContId, "assets/audio/audio_quest1_01.mp3");
 
+		// ==== //
+		// Fred //
+		// ==== //
 		fredContId = SimpleText.database.addContribution(questID, 
 														"Fred", 
 						"I'm lost. Please find my map.");
@@ -56,9 +71,85 @@ function fillDatabase(){
 			}
 			SimpleText.database.addComment(questID, fredContId, fComNames[j], fComTexts[j]);
 		}
+		
+		var currContId;
+		// ========= //
+		// WaddleCat //
+		// ========= //
+		currContId = SimpleText.database.addContribution(questID, 
+											"WaddleCat", 
+											"I’m lost, I don’t know how I got here.\n" +
+											"Please help me find my map.\n" +
+											"I want to go home.");
+		for (var j=0; j < 4; ++j){
+			SimpleText.database.addVote(questID, currContId);
+		}
+
+		// ======= //
+		// DogTaco //
+		// ======= //
+		currContId = SimpleText.database.addContribution(questID, 
+											"DogTaco",
+											"Help me I’m lost! I don’t know how I ended up here. " +
+											"My camp is near a lake.\nTo find my camp I need my map." +
+											"Please help me find my map.");
+		for (var j=0; j < 3; ++j){
+			SimpleText.database.addVote(questID, currContId);
+		}
+		
+		// ========= //
+		// FishJudge //
+		// ========= //
+		currContId = SimpleText.database.addContribution(questID, 
+											"FishJudge",
+											"I have lost the way to my camp.\n" +
+											"My camp is near a lake.\n" +
+											"We came to the forest from the north.\n" +
+											"Help me find my map so I can go back to the camp.");
+
+		for (var j=0; j < 3; ++j){
+			SimpleText.database.addVote(questID, currContId);
+		}
+
+		// ========== //
+		// RatThrower //
+		// ========== //
+		currContId = SimpleText.database.addContribution(questID, 
+											"RatThrower",
+											"I need your help, I have lost my map.\n"+
+											"Please help me find my map so I can go back to my camp.\n" +
+											"My camp is near a lake.\n" +
+											"I don’t like it here, I want my map so I can go home.");
+
+		for (var j=0; j < 2; ++j){
+			SimpleText.database.addVote(questID, currContId);
+		}
+
+		// ========== //
+		// HostilePen //
+		// ========== //
+		currContId = SimpleText.database.addContribution(questID, 
+											"HostilePen",
+											"I stumbled into this forest and can’t find the way back to my camp.\n"+
+											"My camp is near a lake.\n"+
+											"I walked into this forest from the north side.\n"+
+											"Please help me find my map!");
+		for (var j=0; j < 1; ++j){
+			SimpleText.database.addVote(questID, currContId);
+		}
 	 } // populateQuest1
 
+	/**
+	* Populates database with contributions for quest 2.
+	*
+	* @param [string] questID
+	* Quest ID for quest 2.
+	*/
 	function populateQuest2(questID){
+		
+		// ===== //
+		// Nisse //
+		// ===== //
 		nisseContId = SimpleText.database.addContribution(questID, 
 														"Nisse", 
 				"You see a map under a rock. Someone has placed it there. " + 
@@ -83,12 +174,18 @@ function fillDatabase(){
 			SimpleText.database.addComment(questID, nisseContId, comNames[i], comTexts[i]);
 		}
 
+		// === //
+		// óle //
+		// === //
 		oleContId = SimpleText.database.addContribution(questID, 
 														"óle", 
 														"Run in circles.");
 		SimpleText.database.addComment(questID, oleContId, "Fred", "No, don't listen to him!");
 		SimpleText.database.addComment(questID, oleContId, "Hugo", "That isn't what the text said.");
 
+		// ==== //
+		// Fred //
+		// ==== //
 		fredContId = SimpleText.database.addContribution(questID, 
 														"Fred", 
 										"You found a dirty map. " +
@@ -104,6 +201,7 @@ function fillDatabase(){
 			SimpleText.database.addVote(questID, fredContId);
 			SimpleText.database.addComment(questID, fredContId, fComNames[j], fComTexts[j]);
 		}
+
 	} // populateQuest2
 
 	populateAuthors();
